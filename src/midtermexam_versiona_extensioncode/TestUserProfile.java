37c310176387e8d15fa50551d4facb17d3dec90c
hafsakhan94-123 <hafsa.khan73@gmail.com>
2022-06-21 12:52:31 -0400

    Midterm commit2.0

---
+++ src/midtermexam_versiona_extensioncode/TestUserProfile.java
@@ -0,0 +1,9 @@
+package midtermexam_versiona_extensioncode;
+
+public class TestUserProfile {
+    public static void main(String[] args) {
+        UserProfile userProfile = new UserProfile("123","Action");
+
+
+    }
+}
--- src/midtermexam_versiona_extensioncode/UserProfile.java
+++ src/midtermexam_versiona_extensioncode/UserProfile.java
@@ -29,6 +29,7 @@ public class UserProfile
     {
         userID = givenID;
         genre= givenGenre;
+
     }
 
     /**
